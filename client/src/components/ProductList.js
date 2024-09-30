935fe5d582a309061a34db3aeb8e642e2df757b9
0knoSakr <sakura.okano@gmail.com>
2024-09-30 12:59:36 +0900

    さらにuseCallbackを使用して修正

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,47 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import api from '../services/api'; // api.jsをインポート
-import { Link } from 'react-router-dom';
-import { fetchProducts } from '../services/productService';
+import React, { useState, useEffect, useCallback } from "react";
+import { Link } from "react-router-dom";
+import { fetchProducts } from "../services/productService";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
-  const [items, setItems] = useState('');
-  const [sku, setSku] = useState('');
+  const [items, setItems] = useState("");
+  const [sku, setSku] = useState("");
 
   //商品をロードする関数
-  const LoadProducts = async () => {
+  const loadProducts = useCallback(async () => {
     try {
       const result = await fetchProducts(items, sku);
       setProducts(result);
     } catch (error) {
       console.error("商品情報の取得に失敗しました:", error);
     }
-  };
+  },[items, sku]);
 
   //コンポーネントがマウントされたときとitemsまたはskuが変更されたとき
   //商品をロード
   useEffect(() => {
-    LoadProducts();
-  }, [items, sku]);
+    loadProducts();
+  }, [loadProducts]);
 
   return (
     <div>
       <h2>商品リスト</h2>
-      <input type="text"
+      <input
+        type="text"
         value={items}
         onChange={(e) => setItems(e.target.value)}
+        placeholder="商品名"
       />
-      <button>商品名で検索</button>
-      <input type="text"
+      <button onClick={loadProducts}>商品名で検索</button>
+      <input
+        type="text"
         value={sku}
         onChange={(e) => setSku(e.target.value)}
+        placeholder="SKU"
       />
-      <button>SKUで検索</button>
+      <button onClick={loadProducts}>SKUで検索</button>
       <ul>
         {products.map((product) => (
           <li key={product.id}>
             {product.name} - 在庫: {product.stock}
-            <button><Link to={`/products/${product.id}`}>詳細へ</Link></button>
+            <button>
+              <Link to={`/products/${product.id}`}>詳細へ</Link>
+            </button>
           </li>
         ))}
       </ul>
