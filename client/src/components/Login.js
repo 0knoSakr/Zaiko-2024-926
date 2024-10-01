9d786e14aae44297dec669fe15bc35c3ea00be28
0knoSakr <sakura.okano@gmail.com>
2024-10-01 16:20:35 +0900

    Loginコンポーネントファイルを作成

---
+++ client/src/components/Login.js
@@ -0,0 +1,11 @@
+const Login = () => {
+  return (
+    <form>
+      <input type="text" placeholder="Username" />
+      <input type="Password" placeholder="Password" />
+      <button type="submit">ログイン</button>
+    </form>
+  )
+}
+
+export default Login;
